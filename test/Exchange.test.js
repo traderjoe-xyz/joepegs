921280e95ad233c5dc28e77b2dc2ad33baee15f2
0xnivek <0xnivek@gmail.com>
2022-03-05 19:51:01 -0500

    Setup contract deployments in test

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -9,19 +9,25 @@ describe("Exchange", function () {
     this.ExecutionManagerCF = await ethers.getContractFactory(
       "ExecutionManager"
     );
-    this.ExchangeCF = await ethers.getContractFactory("LooksRareExchange");
     this.OrderBookCF = await ethers.getContractFactory("OrderBook");
-    this.RoyaltyFeeManagerCF = await ethers.getContractFactory(
-      "RoyaltyFeeManager"
+    this.RoyaltyFeeRegistryCF = await ethers.getContractFactory(
+      "RoyaltyFeeRegistry"
     );
     this.RoyaltyFeeSetterCF = await ethers.getContractFactory(
       "RoyaltyFeeSetter"
     );
+    this.RoyaltyFeeManagerCF = await ethers.getContractFactory(
+      "RoyaltyFeeManager"
+    );
     this.StrategyStandardSaleForFixedPriceCF = await ethers.getContractFactory(
       "StrategyStandardSaleForFixedPrice"
     );
+    this.ExchangeCF = await ethers.getContractFactory("LooksRareExchange");
     this.TransferManagerERC721CF = await ethers.getContractFactory(
       "TransferManagerERC721"
+    );
+    this.TransferManagerERC1155CF = await ethers.getContractFactory(
+      "TransferManagerERC1155"
     );
     this.TransferSelectorNFTCF = await ethers.getContractFactory(
       "TransferSelectorNFT"
@@ -34,10 +40,47 @@ describe("Exchange", function () {
     this.carol = this.signers[3];
   });
 
-  beforeEach(async function () {});
+  beforeEach(async function () {
+    this.erc721Token = await this.ERC721TokenCF.deploy();
+    this.currencyManager = await this.CurrencyManagerCF.deploy();
+    this.executionManager = await this.ExecutionManagerCF.deploy();
+    this.orderBook = await this.OrderBookCF.deploy();
+    this.royaltyFeeLimit = 1000; // 1000 = 10%
+    this.royaltyFeeRegistry = await this.RoyaltyFeeRegistryCF.deploy(
+      this.royaltyFeeLimit
+    );
+    this.royaltyFeeSetter = await this.RoyaltyFeeSetterCF.deploy(
+      this.royaltyFeeRegistry.address
+    );
+    this.royaltyFeeManager = await this.RoyaltyFeeManagerCF.deploy(
+      this.royaltyFeeRegistry.address
+    );
+    this.protocolFee = 100; // 100 = 1%
+    this.strategyStandardSaleForFixedPrice =
+      await this.StrategyStandardSaleForFixedPriceCF.deploy(this.protocolFee);
+    this.WAVAX = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
+    this.exchange = await this.ExchangeCF.deploy(
+      this.currencyManager.address,
+      this.executionManager.address,
+      this.royaltyFeeManager.address,
+      this.WAVAX,
+      this.dev.address, // protocolFeeRecipient
+      this.orderBook.address
+    );
+    this.transferManagerERC721 = await this.TransferManagerERC721CF.deploy(
+      this.exchange.address
+    );
+    this.transferManagerERC1155 = await this.TransferManagerERC1155CF.deploy(
+      this.exchange.address
+    );
+    this.transferSelectorNFT = await this.TransferSelectorNFTCF.deploy(
+      this.transferManagerERC721.address,
+      this.transferManagerERC1155.address
+    );
+  });
 
-  describe("", function () {
-    it("", async function () {});
+  describe("test", function () {
+    it("test", async function () {});
   });
 
   after(async function () {
