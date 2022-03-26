9a867390243de9968e97c945f2fa24d9063b6d13
0xnivek <0xnivek@gmail.com>
2022-03-26 12:21:15 -0400

    Address PR feedback

--- deploy/CurrencyManager.js
+++ deploy/CurrencyManager.js
@@ -1,5 +1,3 @@
-const { WAVAX } = require("@traderjoe-xyz/sdk");
-
 module.exports = async function ({
   deployments,
   getChainId,
@@ -10,11 +8,21 @@ module.exports = async function ({
 
   const chainId = await getChainId();
 
-  if (!chainId in WAVAX) {
+  let wavaxAddress;
+
+  if (chainId == 4) {
+    // rinkeby contract addresses
+    wavaxAddress = ethers.utils.getAddress(
+      "0xc778417e063141139fce010982780140aa0cd5ab"
+    ); // wrapped ETH ethers.utils.getAddress
+  } else if (chainId == 43114 || chainId == 31337) {
+    // avalanche mainnet or hardhat network ethers.utils.getAddresses
+    wavaxAddress = ethers.utils.getAddress(
+      "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7"
+    );
+  } else {
     throw new Error("Failed to find WAVAX address");
   }
-
-  const wavaxAddress = WAVAX[chainId].address;
 
   const currencyManager = await deploy("CurrencyManager", {
     from: deployer,
