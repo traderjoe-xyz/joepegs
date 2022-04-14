e663dbfae97cc39682667dc597d6e33943822601
0xnivek <0xnivek@gmail.com>
2022-04-14 19:08:12 -0400

    Add protocol fee manager

---
+++ contracts/interfaces/IProtocolFeeManager.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IProtocolFeeManager {
+    function setDefaultProtocolFee(uint256 _defaultProtocolFee) external;
+
+    function setProtocolFeeForCollection(
+        address _collection,
+        uint256 _protocolFee
+    ) external;
+
+    function unsetProtocolFeeForCollection(address _collection) external;
+
+    function protocolFeeForCollection(address _collection)
+        external
+        view
+        returns (uint256);
+}
