92d6d0ef992d51cabc13e2bd8b7fe4f6bf0d409a
0xnivek <0xnivek@gmail.com>
2022-04-16 15:26:55 -0400

    Add mock tokenURI and deploy script for ERC721Token

--- contracts/mocks/ERC721Token.sol
+++ contracts/mocks/ERC721Token.sol
@@ -7,9 +7,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import {IERC165, IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-/// @title Joe Token, JOE
+/// @title Mock ERC721 Token
 /// @author Trader Joe
-/// @dev ONLY FOR TESTS
 contract ERC721Token is ERC721("Sample NFT", "NFT"), Ownable, IERC2981 {
     using Counters for Counters.Counter;
 
@@ -18,15 +17,25 @@ contract ERC721Token is ERC721("Sample NFT", "NFT"), Ownable, IERC2981 {
 
     Counters.Counter private _tokenIds;
 
-    /// @dev Mint _amount to _to. Callable only by owner
+    /// @dev Mint _amount to _to
     /// @param _to The address that will receive the mint
-    function mint(address _to) external onlyOwner returns (uint256) {
+    function mint(address _to) external returns (uint256) {
         _tokenIds.increment();
 
         uint256 newTokenId = _tokenIds.current();
         _mint(_to, newTokenId);
 
         return newTokenId;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        pure
+        override
+        returns (string memory)
+    {
+        return
+            "https://azuki-builder.s3.amazonaws.com/images/fcb92224-2223-4210-aaa8-50d5c3cc966a.png";
     }
 
     /**
