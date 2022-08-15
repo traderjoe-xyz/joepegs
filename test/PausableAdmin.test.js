c120df12b2c01b43b0e768846aece9683c72caee
0x0Louis <louis@joepegs.com>
2022-08-15 03:37:37 +0200

    Also transfer the PauseAdmin from the previousOwner to the new owner

--- test/PausableAdmin.test.js
+++ test/PausableAdmin.test.js
@@ -83,6 +83,62 @@ describe("BatchTransferNFT", function () {
     ).to.be.revertedWith("PendingOwnable__NotOwner");
   });
 
+  it("Should add new owner to admin and remove the privilege of the previous one", async function () {
+    this.pausableAdmin.connect(this.dev).setPendingOwner(this.alice.address);
+
+    await expect(
+      this.pausableAdmin.connect(this.dev).becomeOwner()
+    ).to.be.revertedWith("PendingOwnable__NotPendingOwner");
+
+    await expect(
+      this.pausableAdmin.connect(this.bob).becomeOwner()
+    ).to.be.revertedWith("PendingOwnable__NotPendingOwner");
+
+    this.pausableAdmin.connect(this.alice).becomeOwner();
+
+    expect(
+      await this.pausableAdmin.isPauseAdmin(this.alice.address)
+    ).to.be.equal(true);
+
+    expect(await this.pausableAdmin.isPauseAdmin(this.dev.address)).to.be.equal(
+      false
+    );
+
+    this.pausableAdmin.connect(this.alice).addPauseAdmin(this.bob.address);
+
+    await expect(
+      this.pausableAdmin.connect(this.dev).addPauseAdmin(this.dev.address)
+    ).to.be.revertedWith("PendingOwnable__NotOwner");
+
+    await expect(
+      this.pausableAdmin.connect(this.dev).removePauseAdmin(this.alice.address)
+    ).to.be.revertedWith("PendingOwnable__NotOwner");
+
+    this.pausableAdmin.connect(this.alice).removePauseAdmin(this.bob.address);
+  });
+
+  it.only("Should transfer ownership, add the new owner and remove the previous one in all cases", async function () {
+    // new owner isn't admin, previous owner is admin (0,1)
+    await transferOwnershipAndChecks(this.pausableAdmin, this.alice, this.dev);
+
+    // new owner is admin, previous owner is admin (1,1)
+    await this.pausableAdmin
+      .connect(this.alice)
+      .addPauseAdmin(this.dev.address);
+    await transferOwnershipAndChecks(this.pausableAdmin, this.dev, this.alice);
+
+    // new owner is admin, previous owner isn't admin (1,0)
+    await this.pausableAdmin
+      .connect(this.dev)
+      .addPauseAdmin(this.alice.address);
+    await this.pausableAdmin.connect(this.dev).renouncePauseAdmin();
+    await transferOwnershipAndChecks(this.pausableAdmin, this.alice, this.dev);
+
+    // new owner is admin, previous owner is admin (0,0)
+    await this.pausableAdmin.connect(this.alice).renouncePauseAdmin();
+    await transferOwnershipAndChecks(this.pausableAdmin, this.dev, this.alice);
+  });
+
   after(async function () {
     await network.provider.request({
       method: "hardhat_reset",
@@ -90,3 +146,16 @@ describe("BatchTransferNFT", function () {
     });
   });
 });
+
+const transferOwnershipAndChecks = async (
+  contract,
+  newOwner,
+  previousOwner
+) => {
+  await contract.connect(previousOwner).setPendingOwner(newOwner.address);
+  await contract.connect(newOwner).becomeOwner();
+
+  expect(await contract.isPauseAdmin(newOwner.address)).to.be.equal(true);
+
+  expect(await contract.isPauseAdmin(previousOwner.address)).to.be.equal(false);
+};
