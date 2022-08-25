760afb7ec9a9b873158c1691d331ac223e9bfbb3
0xnivek <0xnivek@gmail.com>
2022-08-25 17:09:57 -0400

    Add RoyaltyFeeTypes library

---
+++ contracts/libraries/RoyaltyFeeTypes.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title RoyaltyFeeTypes
+ * @notice This library contains types related to royalty fees
+ */
+library RoyaltyFeeTypes {
+    struct FeeInfoPart {
+        address receiver;
+        uint256 fee;
+    }
+}
