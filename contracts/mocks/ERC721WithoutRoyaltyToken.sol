eb5c3a1ccb566f17b698689ec25c0d808e6e3c58
0xnivek <0xnivek@gmail.com>
2022-08-30 14:41:58 -0400

    Add test/RoyaltyFeeSetterV2.test.js

---
+++ contracts/mocks/ERC721WithoutRoyaltyToken.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/// @title Mock ERC721 Token that doesn't support ERC2981
+/// @author Trader Joe
+contract ERC721WithoutRoyaltyToken is ERC721("Sample NFT", "NFT"), Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    /// @dev Mint a NFT to `_to`
+    /// @param _to The address that will receive the mint
+    /// @return the `tokenId` of the newly minted NFT
+    function mint(address _to) external returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+        _mint(_to, newTokenId);
+
+        return newTokenId;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        pure
+        override
+        returns (string memory)
+    {
+        return
+            "https://ikzttp.mypinata.cloud/ipfs/QmQFkLSQysj94s5GvTHPyzTxrawwtjgiiYS2TBLgrvw8CW/5629";
+    }
+
+    function admin() external view returns (address) {
+        return owner();
+    }
+}
---
+++ test/RoyaltyFeeSetterV2.test.js
@@ -0,0 +1,169 @@
+const { ethers, network } = require("hardhat");
+const { expect } = require("chai");
+const { describe } = require("mocha");
+
+describe.only("RoyaltyFeeSetterV2", function () {
+  let royaltyFeeManager;
+  let royaltyFeeRecipient1;
+  let royaltyFeeRecipient2;
+  let royaltyFeePct1;
+  let royaltyFeePct2;
+
+  const tokenId = 1;
+  const amount = ethers.utils.parseEther("1");
+
+  before(async function () {
+    this.ERC721TokenCF = await ethers.getContractFactory("ERC721Token");
+    this.ERC721WithoutRoyaltyTokenCF = await ethers.getContractFactory(
+      "ERC721WithoutRoyaltyToken"
+    );
+    this.RoyaltyFeeRegistryCF = await ethers.getContractFactory(
+      "RoyaltyFeeRegistry"
+    );
+    this.RoyaltyFeeRegistryV2CF = await ethers.getContractFactory(
+      "RoyaltyFeeRegistryV2"
+    );
+    this.RoyaltyFeeSetterCF = await ethers.getContractFactory(
+      "RoyaltyFeeSetter"
+    );
+    this.RoyaltyFeeSetterV2CF = await ethers.getContractFactory(
+      "RoyaltyFeeSetterV2"
+    );
+    this.RoyaltyFeeManagerCF = await ethers.getContractFactory(
+      "RoyaltyFeeManager"
+    );
+
+    this.signers = await ethers.getSigners();
+    this.dev = this.signers[0];
+    this.alice = this.signers[1];
+    this.bob = this.signers[2];
+    this.carol = this.signers[3];
+    this.david = this.signers[4];
+    this.eric = this.signers[5];
+
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: config.networks.avalanche.url,
+          },
+          live: false,
+          saveDeployments: true,
+          tags: ["test", "local"],
+        },
+      ],
+    });
+  });
+
+  beforeEach(async function () {
+    this.erc721Token = await this.ERC721TokenCF.deploy();
+    this.erc721WithoutRoyaltyToken =
+      await this.ERC721WithoutRoyaltyTokenCF.deploy();
+
+    this.royaltyFeeLimit = 1000; // 1000 = 10%
+    this.royaltyFeeRegistry = await this.RoyaltyFeeRegistryCF.deploy();
+    await this.royaltyFeeRegistry.initialize(this.royaltyFeeLimit);
+
+    this.maxNumRecipients = 2;
+    this.royaltyFeeRegistryV2 = await this.RoyaltyFeeRegistryV2CF.deploy();
+    await this.royaltyFeeRegistryV2.initialize(
+      this.royaltyFeeLimit,
+      this.maxNumRecipients
+    );
+
+    this.royaltyFeeSetter = await this.RoyaltyFeeSetterCF.deploy();
+    await this.royaltyFeeSetter.initialize(this.royaltyFeeRegistry.address);
+    await this.royaltyFeeRegistry.transferOwnership(
+      this.royaltyFeeSetter.address
+    );
+
+    this.royaltyFeeSetterV2 = await this.RoyaltyFeeSetterV2CF.deploy();
+    await this.royaltyFeeSetterV2.initialize(this.royaltyFeeRegistryV2.address);
+    await this.royaltyFeeRegistryV2.transferOwnership(
+      this.royaltyFeeSetterV2.address
+    );
+
+    this.royaltyFeeManager = await this.RoyaltyFeeManagerCF.deploy();
+    royaltyFeeManager = this.royaltyFeeManager;
+    await this.royaltyFeeManager.initialize(this.royaltyFeeRegistry.address);
+    await this.royaltyFeeManager.updateRoyaltyFeeRegistryV2(
+      this.royaltyFeeRegistryV2.address
+    );
+
+    // Royalty fee information for RoyaltyFeeSetterV2
+    this.royaltyFeeRecipient1 = this.david.address;
+    royaltyFeeRecipient1 = this.royaltyFeeRecipient1;
+    this.royaltyFeePct1 = 500;
+    royaltyFeePct1 = this.royaltyFeePct1;
+    this.royaltyFeeRecipient2 = this.eric.address;
+    royaltyFeeRecipient2 = this.royaltyFeeRecipient2;
+    this.royaltyFeePct2 = 100;
+    royaltyFeePct2 = this.royaltyFeePct2;
+  });
+
+  const assertRoyaltyInfoPartsSet = async (collection) => {
+    const feeAmountParts =
+      await royaltyFeeManager.calculateRoyaltyFeeAmountParts(
+        collection,
+        tokenId,
+        amount
+      );
+    expect(feeAmountParts.length).to.be.equal(2);
+
+    expect(royaltyFeeRecipient1).to.be.equal(feeAmountParts[0].receiver);
+    expect(amount.mul(royaltyFeePct1).div(10_000)).to.be.equal(
+      feeAmountParts[0].amount
+    );
+
+    expect(royaltyFeeRecipient2).to.be.equal(feeAmountParts[1].receiver);
+    expect(amount.mul(royaltyFeePct2).div(10_000)).to.be.equal(
+      feeAmountParts[1].amount
+    );
+  };
+
+  describe("updateRoyaltyInfoPartsForCollectionIfAdmin", function () {
+    it("cannot update if supports ERC2981", async function () {
+      await expect(
+        this.royaltyFeeSetterV2.updateRoyaltyInfoPartsForCollectionIfAdmin(
+          this.erc721Token.address,
+          this.dev.address,
+          [{ receiver: this.alice.address, fee: 100 }]
+        )
+      ).to.be.revertedWith(
+        "RoyaltyFeeSetterV2__CollectionCannotSupportERC2981"
+      );
+    });
+
+    it("cannot update if not admin", async function () {
+      await expect(
+        this.royaltyFeeSetterV2
+          .connect(this.carol)
+          .updateRoyaltyInfoPartsForCollectionIfAdmin(
+            this.erc721WithoutRoyaltyToken.address,
+            this.dev.address,
+            [{ receiver: this.alice.address, fee: 100 }]
+          )
+      ).to.be.revertedWith("RoyaltyFeeSetterV2__NotCollectionAdmin");
+    });
+
+    it("can successfully update", async function () {
+      await this.royaltyFeeSetterV2.updateRoyaltyInfoPartsForCollectionIfAdmin(
+        this.erc721WithoutRoyaltyToken.address,
+        this.dev.address,
+        [
+          { receiver: this.royaltyFeeRecipient1, fee: this.royaltyFeePct1 },
+          { receiver: this.royaltyFeeRecipient2, fee: this.royaltyFeePct2 },
+        ]
+      );
+      await assertRoyaltyInfoPartsSet(this.erc721WithoutRoyaltyToken.address);
+    });
+  });
+
+  after(async function () {
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [],
+    });
+  });
+});
