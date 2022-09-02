825a389b3499e3d2ef637febd2e7d854125fc6b3
0xnivek <0xnivek@gmail.com>
2022-09-02 14:27:57 -0400

    Update RoyaltyFeeManager deploy script to include royaltyFeeRegistryV2

--- deploy/RoyaltyFeeManager.js
+++ deploy/RoyaltyFeeManager.js
@@ -1,6 +1,10 @@
 const { verify } = require("./utils");
 
-module.exports = async function ({ getNamedAccounts, deployments, getChainId }) {
+module.exports = async function ({
+  getNamedAccounts,
+  deployments,
+  getChainId,
+}) {
   const { deploy, catchUnknownSigner } = deployments;
   const { deployer } = await getNamedAccounts();
 
@@ -16,8 +20,9 @@ module.exports = async function ({ getNamedAccounts, deployments, getChainId }) 
   }
 
   const royaltyFeeRegistry = await deployments.get("RoyaltyFeeRegistry");
+  const royaltyFeeRegistryV2 = await deployments.get("RoyaltyFeeRegistryV2");
 
-  const args = [royaltyFeeRegistry.address];
+  const args = [royaltyFeeRegistry.address, royaltyFeeRegistryV2.address];
   await catchUnknownSigner(async () => {
     proxyContract = await deploy("RoyaltyFeeManager", {
       from: deployer,
@@ -41,4 +46,4 @@ module.exports = async function ({ getNamedAccounts, deployments, getChainId }) 
 };
 
 module.exports.tags = ["RoyaltyFeeManager"];
-module.exports.dependencies = ["RoyaltyFeeRegistry"];
+module.exports.dependencies = ["RoyaltyFeeRegistry", "RoyaltyFeeRegistryV2"];
