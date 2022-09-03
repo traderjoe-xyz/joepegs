ec85f036c1270aa4650800661038f8fe39c11dd3
0xnivek <0xnivek@gmail.com>
2022-09-03 13:26:04 -0400

    Fix pending ownable test

---
+++ contracts/mocks/MockPendingOwnable.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../utils/PendingOwnable.sol";
+
+/// @title Mock contract using `PendingOwnable`
+/// @author Trader Joe
+contract MockPendingOwnable is PendingOwnable {
+
+}
