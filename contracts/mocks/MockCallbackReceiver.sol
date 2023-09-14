ec6f604825c84ada8c36c20ee36a470addfc54ee
Patate <patate@joepegs.com>
2023-09-14 16:19:19 +0200

    add tests

---
+++ contracts/mocks/MockCallbackReceiver.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ICallbackRecipient} from "../interfaces/ICallbackRecipient.sol";
+
+contract MockCallbackReceiver is ICallbackRecipient {
+    /// @dev bytes4(keccak256("isValidSignature(bytes32,bytes)")
+    bytes4 internal constant MAGIC_VALUE = 0x1626ba7e;
+
+    event CallbackCalled();
+
+    function saleCallback(address, bytes32) external override {
+        emit CallbackCalled();
+    }
+
+    function isValidSignature(
+        bytes32,
+        bytes memory
+    ) external pure returns (bytes4) {
+        return MAGIC_VALUE;
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) external pure returns (bytes4) {
+        return MockCallbackReceiver.onERC721Received.selector;
+    }
+}
